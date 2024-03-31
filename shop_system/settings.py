1a6990bd960a74b47a0e354a345a30893a2418ad
shuhrat2001 <ilhomjonovsh@gmail.com>
2024-03-31 17:35:47 +0500

    project created

--- shop_system/settings.py
+++ shop_system/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4&3m55%v0-_fs68!pzb4ss13!^0_mpf0a^*kb#jvm$o@lj*1=0'
+SECRET_KEY = 'django-insecure-*@*2a7#=r!n-#e4jl5u85y-i)49sklw^p#j02__b_e$*9(_&ec'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
